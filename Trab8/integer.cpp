db947066465143551568a0148ee249dbdeb9da4b
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2013-12-05 23:11:05 -0200

    Implementação do operador *

--- Trab8/integer.cpp
+++ Trab8/integer.cpp
@@ -42,6 +42,18 @@ public:
     return Integer( mpz_get_str(NULL, 10, a) );
   }
 
+  Integer operator* ( Integer x ) {
+    mpz_t a, b, c;
+
+    mpz_init( a );
+    mpz_init_set_str( b, st.c_str(), 10 );
+    mpz_init_set_str( c, x.st.c_str(), 10 );
+
+    mpz_mul( a, b, c );
+
+    return Integer( mpz_get_str(NULL, 10, a) );
+  }
+
 private:
   string st;
 };
@@ -61,12 +73,16 @@ Integer operator"" _ai( const char* st ) {
 int main( int argc, char* argv[] ) {
   Integer a = 798747327497297389275473298748937584695475873_ai;
   Integer b = 798747327497297389275473298748937584695475873_ai;
+  Integer c = 2_ai;
   
   cout << "Soma:" << endl;
   cout << a + b << endl << endl;
 
   cout << "Subtração:" << endl;
   cout << a - b << endl << endl;
+
+  cout << "Multiplicação:" << endl;
+  cout << a * c << endl << endl;
 
 
   return 0;
