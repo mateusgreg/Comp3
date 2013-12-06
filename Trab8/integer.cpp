a95ae02c0f8d9a4bc78eb040da5719415c546965
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2013-12-05 22:59:27 -0200

    Criação da classe Integer, que encapsula as funções da bilioteca GMP, e implementação do operador +

---
+++ Trab8/integer.cpp
@@ -0,0 +1,58 @@
+/*
+ * integer.cpp
+ *
+ *  Created on: Nov 29, 2013
+ *      Author: mateus
+ */
+
+#include <iostream>
+#include <gmpxx.h>
+
+using namespace std;
+
+class Integer {
+public:
+  Integer( string st ): st(st) {}
+
+  ostream& print( ostream& o ) const {
+    return o << st;
+  }
+
+  Integer operator+ ( Integer x ) {
+    mpz_t a, b, c;
+
+    mpz_init( a );
+    mpz_init_set_str( b, st.c_str(), 10 );
+    mpz_init_set_str( c, x.st.c_str(), 10 );
+
+    mpz_add( a, b, c );
+
+    return Integer( mpz_get_str(NULL, 10, a) );
+  }
+
+private:
+  string st;
+};
+
+
+ostream& operator << ( ostream& o, const Integer i ) {
+  return i.print( o );
+}
+
+
+// operator user-defined literal
+Integer operator"" _ai( const char* st ) {
+  return Integer( st );
+}
+
+
+int main( int argc, char* argv[] ) {
+  Integer a = 798747327497297389275473298748937584695475873_ai;
+  Integer b = 798747327497297389275473298748937584695475876_ai;
+  
+  cout << "Soma:" << endl;
+  cout << a + b << endl;
+
+
+  return 0;
+}
